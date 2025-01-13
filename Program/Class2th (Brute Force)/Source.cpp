81feeac6447b24cddaad0c25744e4931a8801ed2
chikumo <yangju058@gmail.com>
2025-01-13 15:41:39 +0900

    (2025.1.13) 시간복잡도 & 공간복잡도

---
+++ Program/Class2th (Brute Force)/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+#pragma region 브루트 포스
+	// 가능한 모든 경우의 수를 탐색하면서
+	// 결과를 도출해내는 알고리즘입니다.
+
+	/// 비밀번호 000 ~ 999
+
+	int password[3] = { 1, 2, 3 };
+
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+			for (int k = 0; k <= 9; k++)
+			{
+				if (password[0] == i && password[1] == j && password[2] == k)
+				{
+					cout << i << " " << j << " " << k << endl;
+					cout << "비밀번호 해제" << endl;
+
+					break;
+				}
+			}
+		}
+	}
+
+#pragma endregion
+
+
+	return 0;
+}
---
+++ Program/Class3th (Bubble Sort)/Source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#define SIZE 5
+using namespace std;
+
+
+
+int main()
+{
+#pragma region 거품 정렬
+	// 서로 인접한 두 원소를 비교하여 정렬하는 알고리즘입니다.
+	// 오름차순 정렬
+
+	int array[SIZE] = { 5, 8, 2, 1, 4 };
+
+	// 회차가 거듭할수록 맨 마지막 인덱스의 값이 점차 정렬된다
+	// 정렬이 확정된 뒷부분을 제외한 부분만 정렬해야해서 (SIZE - i) - 1
+
+	// 회차
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		// 안쪽 정렬
+		for (int j = 0; j < (SIZE - i) - 1; j++)
+		{
+			// 왼쪽이 더 크다면 swap
+			if (array[j] > array[j + 1])
+			{
+				swap(array[j], array[j + 1]);
+			}
+		}
+	}
+
+	for (const int& element : array)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
